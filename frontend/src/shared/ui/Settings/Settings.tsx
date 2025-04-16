554feb599c6e0a82fdfe682788276c5ea4b10b9f
artalexandra <alexandraartyushkova@yandex.ru>
2025-04-16 23:24:15 +0300

    feat: created routing

--- frontend/src/shared/ui/Settings/Settings.tsx
+++ frontend/src/shared/ui/Settings/Settings.tsx
@@ -1,12 +1,16 @@
 'use client';
 import Image from "next/image";
 import style from './Settings.module.scss';
+import { useRouter } from "next/navigation";
+import { ROUTES } from "@/shared/config/Routes";
 function Settings() {
+    const router = useRouter();
+
     return(
         <div className={style.settings}>
-            <Image src="/settings/user.svg" alt="user" width={50} height={50}/>
-            <Image src="/settings/message.svg" alt="message" width={50} height={50}/> 
-            <Image src="/settings/setting.svg" alt="setting" width={50} height={50}/> 
+            <Image src="/settings/user.svg" alt="user" width={50} height={50} onClick={()=>router.replace(ROUTES.settings.profile)}/>
+            <Image src="/settings/message.svg" alt="message" width={50} height={50} onClick={()=>router.replace(ROUTES.message.main)}/> 
+            <Image src="/settings/setting.svg" alt="setting" width={50} height={50} onClick={()=>router.replace(ROUTES.settings.profile)}/> 
         </div>
     );
 }
